67cd4154464d92d9a17ff8c1f42e7e29f83ed4ae
swapnil.kamble02@gmail.com <swapnil.kamble02@gmail.com>
2016-06-15 22:12:11 +0530

    inital load

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl:'app/mockTemplate.html' 
+})
+export class AppComponent {
+    
+    title ={name:"Hello World", description :"Test Only"};
+    showValue="Test Value";
+    
+    
+    
+ }
+
+
+
+
+
+
+
+
+
+
+
